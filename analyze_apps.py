14c23ce68c633b38c11034dbc16807717632fee6
Alex Petuschak <alex-swiftify@users.noreply.github.com>
2022-09-05 18:54:29 +0400

    Update analyze_apps.py

--- analyze_apps.py
+++ analyze_apps.py
@@ -5,10 +5,15 @@ import plistlib
 import subprocess
 import re
 import csv
+import os
 
-def app_uses_swift(path):
-    libswiftPath = path / 'Frameworks' / 'libswiftCore.dylib'
-    return libswiftPath.exists()
+def get_swift_version(txt):
+    swift_version_search = re.search("Swift = (.+)", txt)
+    swift_version = "not swift"
+    if swift_version_search:
+        swift_version = swift_version_search.group(1)
+    print(swift_version)
+    return swift_version
 
 def is_executable(file_path):
     file_info = subprocess.run(["file", file_path], capture_output=True)
@@ -19,23 +24,42 @@ def find_executable_in(path):
                    if is_executable(file)]
     return None if len(executables) == 0 else executables[0]
 
-def class_dump(executable_path):
-    header = str(subprocess.run(["class-dump", executable_path], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout)
-    return re.findall("@interface ([\w_]+)\s?:\s?(\w+)", header)
+def get_text_of_executable(executable_path):
+    text = subprocess.run(["ipsw", "macho", "info", executable_path, "--objc"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL).stdout.decode('utf-8')
+    return text
+
+def class_dump(text):
+    protocols = re.findall("@protocol (.+)", text)
+    classes = re.findall("0x.{11} ([^[]\w+) :", text)
+    both = classes + protocols
+    return both
 
 def is_apple_class(class_tuple):
-    prefixes = ["NS", "UI", "CA", "SCN", "SK", "CI", "AB", "ML", "GK", "AV"]
+    prefixes = ["NS", "UI", "CA", "SCN", "SK", "CI", "AB", "ML", "GK", "AV", "MK", "SF", "WK", "AS", "CL"]
     for prefix in prefixes:
         pattern = re.compile(f'{prefix}[A-Z][a-z]+')
-        if pattern.match(class_tuple[0]):
+        if pattern.match(class_tuple):
             return True
     return False
 
-def percentage_classes_in_swift(classes):
+def percentage_classes_in_swift(classes, app_name):
     classes = [item for item in classes if not is_apple_class(item)]
+    fw = open
+    str_classes = "\n".join(classes)
+    save_to_file(str_classes, "all_classes.txt", app_name)
     if len(classes) == 0: return 0.0
-    swift_classes = [item for item in classes if item[0].startswith("_T")]
+    swift_classes = [item for item in classes if item.startswith("_T")]
+    str_swift_classes = "\n".join(swift_classes)
+    save_to_file(str_swift_classes, "swift_classes.txt", app_name)
     return float(len(swift_classes)) / float(len(classes))
+
+def save_to_file(content, file_name, app_name):
+    directory_path = f"./classes/{app_name}/"
+    os.makedirs(directory_path, exist_ok=True)
+    path = f"{directory_path}{file_name}"
+    f = open(path, "w")
+    f.write(content)
+    f.close()
     
 def analyze_app(path):
     results = {}
@@ -50,18 +74,23 @@ def analyze_app(path):
     results['bundle_id'] = bundle_id
     results['sdk'] = infoPlist.get('DTSDKName')
     results['deployment_target'] = infoPlist.get('MinimumOSVersion')
-    results['uses_swift'] = app_uses_swift(path)
+    text = get_text_of_executable(path)
+    classes = class_dump(text)
+    results['swift_version'] = get_swift_version(text)
+    results["uses_swift"] = results["swift_version"] != "not swift"
     executable = find_executable_in(path)
     results['executable'] = executable.name
-    classes = class_dump(executable)
-    results['percentage_swift'] = percentage_classes_in_swift(classes)
+    save_to_file(text, f"{app_name}.m", app_name)
+    results['percentage_swift'] = percentage_classes_in_swift(classes, app_name)
     results['main_binary_uses_swift'] = results['percentage_swift'] > 0
+    results['percentage_swift'] = "{:.1f}".format(results['percentage_swift']  * 100)
     return results
+
 
 apps = [path for path in Path.cwd().iterdir() if path.suffix == '.app']
 
 with open('results.csv', mode='w', newline='') as csv_file:
-    fieldnames = ['app_name', 'bundle_id', 'sdk', 'deployment_target', 'uses_swift', 'percentage_swift', 'main_binary_uses_swift', 'executable']
+    fieldnames = ['app_name', 'bundle_id', 'sdk', 'deployment_target','uses_swift', 'swift_version', 'main_binary_uses_swift', 'percentage_swift', 'executable']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()    
     writer.writerows(map(analyze_app, apps))
